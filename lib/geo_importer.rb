1d26fae8431f000355a90e18ad2f595a228e3f63
Igor Springer <springerigor@gmail.com>
2017-05-08 21:20:38 +0200

    Require all created files in `geo_importer`

--- lib/geo_importer.rb
+++ lib/geo_importer.rb
@@ -1,3 +1,15 @@
-require "geo_importer/version"
-require "dotenv/load"
-require "sequel"
+require 'dotenv/load'
+require 'sequel'
+
+require 'geo_importer/models/geolocation'
+
+require 'geo_importer/validators/country_code_validator'
+require 'geo_importer/validators/country_name_validator'
+require 'geo_importer/validators/ip_validator'
+require 'geo_importer/validators/latitude_validator'
+require 'geo_importer/validators/longitude_validator'
+
+require 'geo_importer/services/import_geolocations_from_csv'
+require 'geo_importer/services/import_geolocation_from_csv'
+
+require 'geo_importer/version'
